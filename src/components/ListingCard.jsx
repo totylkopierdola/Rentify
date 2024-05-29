228451d95c9cb3f00c31da6a6344ba01f0f72e60
totylkopierdola <kudraj7@wp.pl>
2024-05-29 13:54:17 +0200

    carousel images on card item

--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import { Card, CardContent } from './ui';
+import { Card, CardContent } from '@/components/ui';
+import { DollarSign, Pin, Users } from 'lucide-react';
 import { getImageUrl } from '@/lib/utils/images';
+import ListingCardImages from '@/components/ListingCardImages';
 
 const ListingCard = ({ listing }) => {
   return (
     <Card className='w-[320px]'>
-      <img
-        className='h-[200px] w-full rounded-md object-cover'
-        src={getImageUrl(listing.images[0])}
-        alt={listing.name}
-      />
-      <CardContent className='p-4'>
-        <h2 className='mb-0 text-xl font-semibold'>{listing.name}</h2>
+      <ListingCardImages listing={listing} />
+      <CardContent className='flex flex-col gap-2 p-4'>
+        <h2 className='mb-2 text-xl font-semibold'>{listing.name}</h2>
+        <div className='flex items-center gap-2'>
+          <DollarSign className='h-4 w-4 text-primary' />
+          <span className='text-muted-foreground'>
+            <span className='font-bold text-foreground'>{listing.price}</span> /
+            night
+          </span>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Pin className='h-4 w-4 text-primary' />
+          <span className='text-muted-foreground'>{listing.locationId}</span>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Users className='h-4 w-4 text-primary' />
+          <span className='text-muted-foreground'>
+            {listing.maxGuests} Guests
+          </span>
+        </div>
       </CardContent>
     </Card>
   );
