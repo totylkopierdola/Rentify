9643663bb2c02111bf6538b38991cc0da45dc18f
totylkopierdola <kudraj7@wp.pl>
2024-06-18 12:30:01 +0200

    link refix

--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 import { Card, CardContent } from '@/components/ui';
 import { DollarSign, Pencil, Pin, Users } from 'lucide-react';
 import ListingCardImages from '@/components/ListingCardImages';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import ListingFavoriteButton from './ListingFavoriteButton';
 import { useAuth } from './AuthProvider';
 
 const ListingCard = ({ listing }) => {
   const userId = useAuth()?.userLoggedIn?.uid;
   const isCreatedByCurrentUser = listing.createdBy === userId;
+  const navigate = useNavigate();
+
+  const handlePencilClick = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    navigate(`/listings/${listing.id}/edit`);
+  };
 
   return (
     <Card className='group w-[320px]'>
-      <Link to={`/listings/${listing.id}`} className='block'>
+      <Link to={`/listings/${listing.id}`}>
         <div className='relative'>
           <ListingFavoriteButton
             className='absolute right-2 top-2 z-10 h-10 w-10 rounded-full border-none bg-secondary bg-opacity-0 p-2 opacity-0 transition-opacity duration-200 group-hover:bg-secondary group-hover:bg-opacity-50 group-hover:opacity-100'
@@ -21,43 +28,43 @@ const ListingCard = ({ listing }) => {
         </div>
         <div className='relative h-[200px] w-full'>
           {isCreatedByCurrentUser && (
-            <div className='absolute left-2 top-2 z-10 rounded bg-primary px-2 py-1 text-secondary shadow-md'>
-              Your listing
-            </div>
+            <>
+              <div className='absolute left-2 top-2 z-10 rounded bg-primary px-2 py-1 text-secondary shadow-md'>
+                Your listing
+              </div>
+              {/* <Link to={`/listings/${listing.id}/edit`}> */}
+              <div
+                onClick={handlePencilClick}
+                className='absolute bottom-2 right-2 z-20 flex h-10 w-10 scale-[0.9] items-center justify-center rounded-md bg-secondary shadow-sm duration-500 hover:scale-100 hover:transition-all'
+              >
+                <Pencil />
+              </div>
+              {/* </Link> */}
+            </>
           )}
           <ListingCardImages listing={listing} />
         </div>
-      </Link>
-      {isCreatedByCurrentUser && (
-        <Link
-          to={`/listings/${listing.id}/edit`}
-          className='absolute bottom-2 right-2 z-20 flex h-10 w-10 scale-[0.9] items-center justify-center rounded-md bg-secondary shadow-sm duration-500 hover:scale-100 hover:transition-all'
-        >
-          <Pencil />
-        </Link>
-      )}
-      <CardContent className='flex flex-col gap-2 p-4'>
-        <Link to={`/listings/${listing.id}`} className='block'>
+        <CardContent className='flex flex-col gap-2 p-4'>
           <h2 className='mb-2 text-xl font-semibold'>{listing.name}</h2>
-        </Link>
-        <div className='flex items-center gap-2'>
-          <DollarSign className='h-4 w-4 text-primary' />
-          <span className='text-muted-foreground'>
-            <span className='font-bold text-foreground'>{listing.price}</span> /
-            night
-          </span>
-        </div>
-        <div className='flex items-center gap-2'>
-          <Pin className='h-4 w-4 text-primary' />
-          <span className='text-muted-foreground'>{listing.location}</span>
-        </div>
-        <div className='flex items-center gap-2'>
-          <Users className='h-4 w-4 text-primary' />
-          <span className='text-muted-foreground'>
-            {listing.maxGuests} Guests
-          </span>
-        </div>
-      </CardContent>
+          <div className='flex items-center gap-2'>
+            <DollarSign className='h-4 w-4 text-primary' />
+            <span className='text-muted-foreground'>
+              <span className='font-bold text-foreground'>{listing.price}</span>{' '}
+              / night
+            </span>
+          </div>
+          <div className='flex items-center gap-2'>
+            <Pin className='h-4 w-4 text-primary' />
+            <span className='text-muted-foreground'>{listing.location}</span>
+          </div>
+          <div className='flex items-center gap-2'>
+            <Users className='h-4 w-4 text-primary' />
+            <span className='text-muted-foreground'>
+              {listing.maxGuests} Guests
+            </span>
+          </div>
+        </CardContent>
+      </Link>
     </Card>
   );
 };
